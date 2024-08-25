85614dcbd4598efe8b9f0f1d084015000216df7f
sundar uppu <sundaruppu@gmail.com>
2024-08-25 12:31:30 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import App from './App'
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root'),
+// )
+
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client' // Note the change here
 import App from './App'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root')) // Create a root
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 )
